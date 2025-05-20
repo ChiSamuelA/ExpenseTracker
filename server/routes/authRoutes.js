df6802d9e1a23fc95a6f4157bcec0d72416c0960
ChiSamuel651 <chijoe0306@gmail.com>
2025-05-20 16:27:07 +0100

    ✨ feat: added Swagger UI for API documentation at /api-docs: login API

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -27,13 +27,40 @@ const router = express.Router();
  *                 type: string
  *               password:
  *                 type: string
+ *              profileImageUrl:
+ *                 type: string
  *     responses:
  *       201:
  *         description: User registered successfully
  */
-
 router.post('/register', registerUser);
 
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful, returns user and token
+ *       401:
+ *         description: Invalid credentials
+ *       400:
+ *         description: Missing required fields
+ */
 router.post('/login', loginUser);
 
 // router.get('/getUser', protect, getUserInfo);
