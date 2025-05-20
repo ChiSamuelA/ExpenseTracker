fac14aed88f5054a7e09e2e7ad4a705fe8b2170e
ChiSamuel651 <chijoe0306@gmail.com>
2025-05-20 12:22:01 +0100

    🧪 test: /register endpoint now fully tested

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express();
+
+app.use(express.json());
 
 // middleware to handle CORS
 app.use(
@@ -15,6 +19,10 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB()
+
+app.use('/api/v1/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
